199347af4f6fbca4ed1381f0ecb98f929269b043
Lim Chun Leng <lim@logly.co.jp>
2022-06-25 11:14:45 +0900

    add plugins
    - ranger-autojump
    - ranger-devicons2

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,8 @@
 APP_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
 
+mkdir -p ${HOME}/.config/ranger/plugins
 
+ln -sfn ${APP_PATH}/plugins/ranger-devicons2 ${HOME}/.config/ranger/plugins/ranger-devicons2
+ln -sfn ${APP_PATH}/plugins/ranger-autojump/autojump.py ${HOME}/.config/ranger/plugins/autojump.py
 
 ln -sfn ${APP_PATH}/rc.conf ${HOME}/.config/ranger/rc.conf
